d5e4b30fe3166a8cb547509cc3e3d57d63180380
phuonggthao <phuongthao2005ab@gmail.com>
2025-11-05 08:16:44 +0700

    .

--- abchotel/Formlgin.Designer.cs
+++ abchotel/Formlgin.Designer.cs
@@ -58,10 +58,10 @@
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label1.Location = new System.Drawing.Point(163, 43);
+            this.label1.Location = new System.Drawing.Point(158, 43);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 34);
+            this.label1.Size = new System.Drawing.Size(113, 34);
             this.label1.TabIndex = 2;
             this.label1.Text = "LOGIN";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
